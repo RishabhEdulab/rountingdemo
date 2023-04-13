153360e882362ba2f6484b3374fd8a8be21aa4b1
Rishabh <rishabhp@edulab.in>
2023-04-13 11:36:55 +0530

    routing demo example

--- auth/reg.js
+++ auth/reg.js
@@ -1,20 +1,19 @@
-const express = require('express')
-const router = express.Router()
-router.get('/register', (req, res) => {
-    //TODO : register the user in the database
-  res.send('you have been sucessfully registered in the system')
-})
+const express = require("express");
+const router = express.Router();
+router.get("/register", (req, res) => {
+  //TODO : register the user in the database
+  res.send("you have been sucessfully registered in the system");
+});
 
-router.post('/login',(req,res)=>{
+router.post("/login", (req, res) => {
+  console.log("req.body", req.body);
   const username = req.body.username;
   const password = req.body.password;
 
-  if(username == 'admin' && password == 'admin'){
-    res.send("success!")
-  }else 
-  {
-    res.sendStatus(401);//("Login failed!")
+  if (username == "admin" && password == "admin") {
+    res.send("success!");
+  } else {
+    res.sendStatus(401); //("Login failed!")
   }
-
-})
-module.exports = router
+});
+module.exports = router;
--- index.js
+++ index.js
@@ -1,43 +1,42 @@
-const express = require('express')
-const app = express()
-const port = 3001
-const bodyParser = require('body-parser')
-const reg = require('./auth/reg');
+const express = require("express");
+const app = express();
+const port = 3001;
+const bodyParser = require("body-parser");
+const reg = require("./auth/reg");
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // GET method route
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   console.log("get method called ");
-  res.send('GET request to the homepage')
-})
-function abc(req, res, next){
+  res.send("GET request to the homepage");
+});
+function abc(req, res, next) {
   console.log("middle ware called");
   next();
 }
-app.get('/reg/:email', abc,(req, res,next) => {
+app.get("/reg/:email", abc, (req, res, next) => {
   const fname = req.query.fname;
   const lname = req.query.lname;
   const email = req.params.email;
 
-
-  res.send('hello '+ fname + ' ' +lname + ' and your email is '+email)
-})
+  res.send(`hello ${fname} ${lname} ${email}`);
+});
 
 // POST method route
-app.post('/', (req, res) => {
+app.post("/", (req, res) => {
   console.log("post method called ");
-  res.send('POST request to the homepage ')
-})
+  res.send("POST request to the homepage ");
+});
 
-app.use('/auth',reg)
+app.use("/auth", reg);
 
 //localhost:3001/auth/register
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
